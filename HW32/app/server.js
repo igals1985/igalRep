9e6cadb328722b26d33a000ed1bcd8c6afc785bf
igals1985 <35166498+igals1985@users.noreply.github.com>
2018-03-20 17:39:13 +0200

    1

--- HW32/app/server.js
+++ HW32/app/server.js
@@ -3,18 +3,32 @@ var fs = require("fs");  //fs = file-system
 var http = require("http");
 
 var content="<h1>default msg</h1>"
-/*	var date = new Date();
+	var date = new Date();
 	var hour = date.getHours()
-	var x=""
 	console.log(hour)
-		switch(hour){
-			case (hour>=6&&hour<=12):;
-			case (hour>=0&&hour<=1): x= "../html/morning.html"
-		}
-		*/
-fs.readFile("./morning.html", function(err, data){
+if (hour>=6&&hour<=12){
+fs.readFile("../html/morning.html", function(err, data){
 	content=data;
 });
+}
+
+else if (hour>=12&&hour<=16){
+	fs.readFile("../html/afternoon.html", function(err, data){
+		content=data;
+	});
+	}
+
+else if (hour>=16&&hour<=20){
+fs.readFile("../html/evening.html", function(err, data){
+	content=data;
+});
+}
+
+else if (hour>=20&&hour<=6){
+	fs.readFile("../html/night.html", function(err, data){
+		content=data;
+	});
+	}
 
 // Create http server:
 var myServer = http.createServer(function(request, response){
@@ -25,7 +39,7 @@ var myServer = http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 //	var date = new Date();
 //	var hour = date.getHours()
-//	console.log(hour)
+//	
 //	switch(hour){
 //	case (hour>=6&&hour<=12):response.write(contentMorning);
 //	case (hour>=0&&hour<=1):
