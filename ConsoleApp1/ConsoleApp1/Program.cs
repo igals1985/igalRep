b091fa1bca4e599458b04c4a9a4531509ccbe424
igals1985 <35166498+igals1985@users.noreply.github.com>
2018-05-15 00:05:55 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace CharSub
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, write first number");
+            byte a= Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Please write second number");
+            byte b = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"The sum of two numbers is: {a+b}");
+
+            
+        }
+    }
+}
