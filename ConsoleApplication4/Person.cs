c53c32236a5c315549671db01994795d269e017b
igals1985 <35166498+igals1985@users.noreply.github.com>
2018-05-25 13:16:06 +0300

    1
    1

---
+++ ConsoleApplication4/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Person
+    {
+        public Person()
+        {
+            Age = 18;
+            Name = "igal";
+        }
+        public Person(int paramNum=10)
+        {
+            Age = paramNum;
+
+        }
+        public Person(int paramNum=20, string param="dana")
+        {
+            Age = paramNum;
+            Name = param;
+        }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0 && value <= 120)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length >= 2 && value.Length <= 10)
+                {
+                    name = value;
+                }
+
+            }
+        }
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Console.WriteLine($"{person1.Name},{person1.Age}");
+
+       
+        }
+    }
+}
