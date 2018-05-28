562941abab75487d5f46cfa003b58443c5d1d92d
igals1985 <35166498+igals1985@users.noreply.github.com>
2018-05-28 23:52:29 +0300

    1

---
+++ static class/static class/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalcClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"The sum of two int is: {Calc.GetSub (1,2)}");
+            Console.WriteLine($"The sum of Three int is: {Calc.GetSub(1,2,3)}");
+            Console.WriteLine($"The sum of two double is: { Calc.GetSub(1.1,2.2)}");
+
+
+        }
+    }
+}
---
+++ static class/static class/calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CalcClass
+{
+        static class Calc
+        {
+
+            //-------------------------------properties--------------------------
+
+            private static DateTime nowDate;
+
+            //--------------------------------constructor-------------------------
+
+            static Calc()
+            {
+                nowDate = DateTime.Now;
+                Console.WriteLine($"This ia calc first use the date is {nowDate}");
+            }
+
+
+        //-------------------------------functions--------------------------
+
+        public static int GetSub(int s1, int s2)
+            {
+                return s1 + s2;
+            }
+
+        public static int GetSub(int s1, int s2, int s3)
+            {
+                return s1 + s2 + s3;
+            }
+
+
+        public static double GetSub(double s1, double s2)
+            {
+                return s1 + s2;
+            }
+        }
+    }
